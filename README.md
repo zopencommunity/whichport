34eb2d2324a239756091fcfdd913e85d034bd3ae
Haritha D <harithamma.d@ibm.com>
2023-03-23 01:05:17 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+which
+
+Which takes one or more arguments. For each of its arguments it prints to stdout the full path of the executables that would have been executed when this argument had been entered at the shell prompt
---
+++ cicd.groovy
@@ -0,0 +1,6 @@
+node('linux')
+{
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/whichport.git'), string(name: 'PORT_DESCRIPTION', value: 'Which takes one or more arguments. For each of its arguments it prints to stdout the full path of the executables that would have been executed when this argument had been entered at the shell prompt' )]
+  }
+}
