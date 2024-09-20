6170b791f40147c53469d698b0abd0849006a7c4
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:29 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/whichport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/whichport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/whichport.git'), string(name: 'PORT_DESCRIPTION', value: 'Which takes one or more arguments. For each of its arguments it prints to stdout the full path of the executables that would have been executed when this argument had been entered at the shell prompt' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/whichport.git'), string(name: 'PORT_DESCRIPTION', value: 'Which takes one or more arguments. For each of its arguments it prints to stdout the full path of the executables that would have been executed when this argument had been entered at the shell prompt' )]
   }
 }
